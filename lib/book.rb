f1168e51adab4cfd4acd2d37dfda548b440cb23c
Rachel Katz <rachel.katz@flatironschool.com>
2023-01-30 12:31:33 -0500

    solution

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,37 @@
+require 'pry'
+class Book 
+    attr_reader :title
+    attr_accessor :author, :page_count, :genre
 
+    def initialize(title)
+        @title = title
+        @author = nil
+        @page_count = nil
+        @genre = nil
+    end
 
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end 
+
+    # def title 
+    #     @title 
+    # end 
+    # def author=(name)
+    #     @author = name
+    # end 
+    # def author 
+    #     @author 
+    # end 
+
+    # def page_count 
+    #     @page_count
+    # end 
+
+    # def page_count=(num)
+    #     @page_count=num
+    # end 
+    
+end 
+
+#binding.pry
