0a289b4f94d7d386b8d9b389fd6ccbb8f050ebcb
Rachel Katz <rachel.katz@flatironschool.com>
2023-01-30 12:48:50 -0500

    SWBAT

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,14 @@
 
+#SWBAT
 
+#create classes
+#understand public instance attributes
+#understand public class attributes 
+
+#Together we will 
+#create the Book class
+#expand on the Book class to create a class attribute
+#set up the environment
+
+#You will
+#finish the Shoe class
