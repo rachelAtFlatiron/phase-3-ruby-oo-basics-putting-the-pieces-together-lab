92fa1c2547db252de8251fb3a19cf071f0882fb5
Rachel Katz <rachel.katz@flatironschool.com>
2023-01-31 08:59:09 -0500

    environment files

---
+++ bin/run.rb
@@ -0,0 +1,6 @@
+require_relative '../config/environment.rb'
+
+guide = Book.new('Guide')
+galaxy = Book.new('Galaxy')
+
+puts Book.amount
--- lib/book.rb
+++ lib/book.rb
@@ -3,15 +3,21 @@ class Book
     attr_reader :title
     attr_accessor :author, :page_count, :genre
 
+    @@amount = 0
     def initialize(title)
         @title = title
         @author = nil
         @page_count = nil
         @genre = nil
+        @@amount += 1
     end
 
     def turn_page
         puts "Flipping the page...wow, you read fast!"
+    end 
+
+    def self.amount 
+        @@amount 
     end 
 
     # def title 
