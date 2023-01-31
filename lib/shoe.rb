de7245f52d736ec64c9cbb883b093591a176efd1
Rachel Katz <rachel.katz@flatironschool.com>
2023-01-31 11:10:40 -0500

    commit tree a677ebe057ce12490d5fd1a812f3663addf32840

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,17 @@
 # Make your shoe class here!
+
+class Shoe 
+    attr_accessor :brand, :color, :size, :material, :condition
+
+    def initialize(brand)
+        @brand = brand
+        # self needs to have an attr_accessor to read/write
+        # @ will update without attr_accessor
+    end
+
+    def cobble
+        # self and @ are used for the same things
+        puts "Your shoe is as good as new!"
+        @condition = 'new'
+    end 
+end 
